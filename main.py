25212042650d096517a9afab930c4c882d4887ed
LucasViguini <lucasviguinivazzoller@gmail.com>
2025-06-16 18:10:34 -0300

    Add initial FastAPI setup with necessary imports

---
+++ main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI, Form
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+from data import produto_repo
