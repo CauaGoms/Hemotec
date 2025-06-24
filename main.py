416f3223a6dea726150fdf67a480c4cbec993837
CauaGoms <caua.privg@gmail.com>
2025-06-24 16:38:47 -0300

    feat: enforce unique email constraint in usuario table and add email existence check during user registration

--- main.py
+++ main.py
@@ -109,6 +109,10 @@ async def post_cadastro(
     cidade_usuario: str = Form(...),
     senha: str = Form(...)
 ):
+    # Verifica se já existe usuário com esse e-mail
+    if usuario_repo.obter_por_email(email):
+        raise Exception("Já existe uma conta cadastrada com esse e-mail.")
+
     status = 1
     data_cadastro = date.today().isoformat()
     usu = Usuario(0, nome, email, senha, cpf, data_nascimento, status, data_cadastro, rua_usuario, bairro_usuario, cidade_usuario, cep_usuario, telefone)
@@ -168,6 +172,52 @@ async def get_root():
 async def get_root():
     response = templates.TemplateResponse("primeira_doacao.html", {"request": {}})
     return response
+
+# @app.get("/admin/produtos")
+# async def get_produtos():
+#     produtos = produto_repo.obter_todos()
+#     response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
+#     return response
+
+# @app.get("/produtos/{id}")
+# async def get_produto_por_id(id: int):
+#     produto = produto_repo.obter_por_id(id)
+#     response = templates.TemplateResponse("produto.html", {"request": {}, "produto": produto})
+#     return response
+
+# @app.get("/admin/produtos/cadastrar")
+# async def get_produto_cadastrar():
+#     response = templates.TemplateResponse("cadastrar_produto.html", {"request": {}})
+#     return response
+
+# @app.post("/admin/produtos/cadastrar")
+# async def post_produto_cadastrar(
+#     nome: str = Form(...),
+#     descricao: str = Form(...),
+#     preco: float = Form(...),
+#     quantidade: int = Form(...)
+# ):
+#     produto = Produto(0, nome, descricao, preco, quantidade)
+#     id_produto = produto_repo.inserir(produto)
+#     if id_produto == None:
+#         raise Exception("Erro ao inserir produto.")
+#     else:
+#         return RedirectResponse("/produtos", status_code=303)
+    
+
+# @app.get("/admin/clientes")
+# async def get_clientes():
+#     clientes = cliente_repo.obter_todos()
+#     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
+#     return response
+
+
+# @app.get("/admin/formas_pagamento")
+# async def get_formas_pagamento():
+#     formas_pagamento = forma_pagamento_repo.obter_todas()
+#     response = templates.TemplateResponse("formas_pagamento.html", {"request": {}, "formas_pagamento": formas_pagamento})
+#     return response
+
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
