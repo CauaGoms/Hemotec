784cf94fd5d1a369c00e466d73fe8f447e34ffaa
CauaGoms <caua.privg@gmail.com>
2025-06-25 17:10:48 -0300

    Banco de dados finalizado, pronto para testes e fotos dos integrantes em "sobre" inseridos

--- data/sql/prontuario_sql.py
+++ data/sql/prontuario_sql.py
@@ -7,15 +7,20 @@ data_atualizacao INTEGER NOT NULL,
 diabetes INTEGER NOT NULL,
 hipertensao INTEGER NOT NULL,
 cardiopatia INTEGER NOT NULL,
-epilepsia INTEGER NOT NULL,
 cancer INTEGER NOT NULL,
 nenhuma INTEGER NOT NULL,
 outros INTEGER NOT NULL,
-outros_detalhe TEXT NULL,
 medicamentos TEXT NOT NULL,
 fumante TEXT NOT NULL,
 alcool TEXT NOT NULL,
 atividade TEXT NOT NULL,
+jejum TEXT NOT NULL,
+sono TEXT NOT NULL,
+bebida TEXT NOT NULL,
+sintomas_gripais TEXT NOT NULL,
+tatuagem TEXT NOT NULL,
+termos TEXT NOT NULL,
+alerta TEXT NOT NULL,
 FOREIGN KEY (cod_doador) REFERENCES doador(cod_doador)
 )
 """
@@ -23,16 +28,13 @@ FOREIGN KEY (cod_doador) REFERENCES doador(cod_doador)
 INSERIR = """
 INSERT INTO prontuario (
 cod_doador, data_criacao, data_atualizacao, diabetes, hipertensao,
-cardiopatia, epilepsia, cancer, nenhuma, outros, outros_detalhe,
-medicamentos, fumante, alcool, atividade
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+cardiopatia, cancer, nenhuma, outros, medicamentos, fumante, alcool, atividade, jejum, sono, bebida, sintomas_gripais, tatuagem, termos, alerta
+) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
 """
 
 OBTER_TODOS = """
 SELECT p.cod_prontuario, d.cod_doador, p.data_criacao, p.data_atualizacao,
-p.diabetes, p.hipertensao, p.cardiopatia, p.epilepsia, p.cancer,
-p.nenhuma, p.outros, p.outros_detalhe, p.medicamentos,
-p.fumante, p.alcool, p.atividade
+p.diabetes, p.hipertensao, p.cardiopatia, p.cancer, p.nenhuma, p.outros, p.medicamentos, p.fumante, p.alcool, p.atividade, p.jejum, p.sono, p.bebida, p.sintomas_gripais, p.tatuagem, p.termos, p.alerta
 FROM prontuario p, 
 doador d
 WHERE p.cod_doador = d.cod_doador
@@ -41,8 +43,7 @@ WHERE p.cod_doador = d.cod_doador
 UPDATE = """
 UPDATE prontuario
 SET cod_doador = ?, data_criacao = ?, data_atualizacao = ?, diabetes = ?, hipertensao = ?,
-cardiopatia = ?, epilepsia = ?, cancer = ?, nenhuma = ?, outros = ?, outros_detalhe = ?,
-medicamentos = ?, fumante = ?, alcool = ?, atividade = ?
+cardiopatia = ?, cancer = ?, nenhuma = ?, outros = ?, medicamentos = ?, fumante = ?, alcool = ?, atividade = ?, jejum = ?, sono = ?, bebida = ?, sintomas_gripais = ?, tatuagem = ?, termos = ?, alerta = ?
 WHERE cod_prontuario = ?;
 """
 
@@ -52,10 +53,7 @@ WHERE cod_prontuario = ?;
 """
 
 OBTER_POR_ID = """
-SELECT p.cod_prontuario, d.cod_doador, p.data_criacao, p.data_atualizacao,
-p.diabetes, p.hipertensao, p.cardiopatia, p.epilepsia, p.cancer,
-p.nenhuma, p.outros, p.outros_detalhe, p.medicamentos,
-p.fumante, p.alcool, p.atividade
+SELECT p.cod_prontuario, d.cod_doador, p.data_criacao, p.data_atualizacao, p.diabetes, p.hipertensao, p.cardiopatia, p.cancer, p.nenhuma, p.outros, p.medicamentos, p.fumante, p.alcool, p.atividade, p.jejum, p.sono, p.bebida, p.sintomas_gripais, p.tatuagem, p.termos, p.alerta
 FROM prontuario p, 
 doador d
 WHERE p.cod_doador = d.cod_doador
