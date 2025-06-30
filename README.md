9662a94fd1b420806e6ca9d64cb0471c6e521896
KalelAxis <kalelaxis@gmail.com>
2025-06-30 01:34:56 -0300

    feat: atualizar exemplos de fixtures para simplificar a criação de dados de teste

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Próximos Passos
 
+- [ ] Colocar blocos with nos repos de acordo com usuario_repo(do Maróquio, mais confiável)
+- [ ] Perguntar Maróquio como fazer testes com herança
+- [ ] Finalizar @pytest.fixture do tests.conftest.py
+- [ ] Ver o que precisa fazer no novo modo de atualizar senha(ver no usuario_repo e util.auth do Maróquio)
+- [ ] Terminar test_usuario_repo para usar de base para o restante dos testes
 - [ ] Finalizar testes do back para entrega;
-- [ ] Mudar nome para database.py no lugar de util.py
 - [ ] Ajeitar o login e o cadastro;
 - [ ] Aplicar as coordenadas das unidades presentes no banco no maps do `boas_vindas_inicio`
 - [ ] Criar os inserts para preencher o banco de dados. Preencher o banco de dados.
--- tests/conftest.py
+++ tests/conftest.py
@@ -66,19 +66,19 @@ def lista_usuarios_exemplo():
     usuarios = []
     for i in range(1, 11):
         usuario = Usuario(
-            cod_usuario=0,
-            nome=f'nome {i:02d}',
-            email=f'email {i:02d}',
-            senha=f'senha {i:02d}',
-            cpf=f'cpf {i:02d}',
-            data_nascimento=datetime(2025, 1, i).date(),
-            status=True,
-            data_cadastro=datetime(2025, 1, i).date(),
-            rua_usuario=f'rua_usuario {i:02d}',
-            bairro_usuario=f'bairro_usuario {i:02d}',
-            cidade_usuario=i,  # Supondo que a cidade tenha o ID igual ao índice
-            cep_usuario=f'cep_usuario {i:02d}',
-            telefone=f'telefone {i:02d}'
+            0,
+            f'nome {i:02d}',
+            f'email {i:02d}',
+            f'senha {i:02d}',
+            f'cpf {i:02d}',
+            datetime(2025, 1, i).date(),
+            True,
+            datetime(2025, 1, i).date(),
+            f'rua_usuario {i:02d}',
+            f'bairro_usuario {i:02d}',
+            i,  # Supondo que a cidade tenha o ID igual ao índice
+            f'cep_usuario {i:02d}',
+            f'telefone {i:02d}'
         )
         usuarios.append(usuario)
     return usuarios
@@ -87,22 +87,22 @@ def lista_usuarios_exemplo():
 def gestor_exemplo():
     from data.model.gestor_model import Gestor
     gestor = Gestor(
-        cod_gestor=1,
-        cod_usuario=0,
-        nome="nome teste",
-        email="email teste",
-        senha="senha teste",
-        cpf="cpf teste",
-        data_nascimento=datetime(2025, 1, 1).date(),
-        status=True,
-        data_cadastro=datetime(2025, 1, 1).date(),
-        rua_usuario="rua_usuario teste",
-        bairro_usuario="bairro_usuario teste",
-        cidade_usuario=1, 
-        cep_usuario="cep_usuario teste",
-        telefone="telefone teste",
-        cnpj="cnpj teste",
-        instituicao="instituicao teste"
+        1,
+        0,
+        "nome teste",
+        "email teste",
+        "senha teste",
+        "cpf teste",
+        datetime(2025, 1, 1).date(),
+        True,
+        datetime(2025, 1, 1).date(),
+        "rua_usuario teste",
+        "bairro_usuario teste",
+        1, 
+        "cep_usuario teste",
+        "telefone teste",
+        "cnpj teste",
+        "instituicao teste"
     )
     return gestor
 
@@ -112,21 +112,21 @@ def lista_gestores_exemplo():
     gestores = []
     for i in range(1, 11):
         gestor = Gestor(
-            cod_gestor=i,
-            nome=f'nome {i:02d}',
-            email=f'email {i:02d}',
-            senha=f'senha {i:02d}',
-            cpf=f'cpf {i:02d}',
-            data_nascimento=datetime(2025, 1, i).date(),
-            status=True,
-            data_cadastro=datetime(2025, 1, i).date(),
-            rua_gestor=f'rua_gestor {i:02d}',
-            bairro_gestor=f'bairro_gestor {i:02d}',
-            cidade_gestor=i,  # Supondo que a cidade tenha o ID igual ao índice
-            cep_gestor=f'cep_gestor {i:02d}',
-            telefone=f'telefone {i:02d}',
-            cnpj=f'cnpj {i:02d}',
-            instituicao=f'instituicao {i:02d}'
+            i,
+            f'nome {i:02d}',
+            f'email {i:02d}',
+            f'senha {i:02d}',
+            f'cpf {i:02d}',
+            datetime(2025, 1, i).date(),
+            True,
+            datetime(2025, 1, i).date(),
+            f'rua_gestor {i:02d}',
+            f'bairro_gestor {i:02d}',
+            i,  # Supondo que a cidade tenha o ID igual ao índice
+            f'cep_gestor {i:02d}',
+            f'telefone {i:02d}',
+            f'cnpj {i:02d}',
+            f'instituicao {i:02d}'
         )
         gestores.append(gestor)
     return gestores
@@ -135,16 +135,16 @@ def lista_gestores_exemplo():
 def instituicao_exemplo():
     from data.model.instituicao_model import Instituicao
     instituicao = Instituicao(
-        cnpj="cnpj teste", 
-        cod_gestor=1, 
-        cod_assinatura=1,
-        nome="nome teste",
-        email="email teste",
-        rua_instituicao="rua_instituicao teste",
-        bairro_instituicao="bairro_instituicao teste",
-        cidade_instituicao=1,
-        cep_instituicao="cep_instituicao teste",
-        telefone="telefone teste"
+        "cnpj teste", 
+        1, 
+        1,
+        "nome teste",
+        "email teste",
+        "rua_instituicao teste",
+        "bairro_instituicao teste",
+        1,
+        "cep_instituicao teste",
+        "telefone teste"
         )
     return instituicao
 
@@ -154,16 +154,16 @@ def lista_instituicoes_exemplo():
     instituicoes = []
     for i in range(1, 11):
         instituicao = Instituicao(
-            cnpj=f'cnpj {i:02d}', 
-            cod_gestor=i, 
-            cod_assinatura=i,
-            nome=f'nome {i:02d}',
-            email=f'email {i:02d}',
-            rua_instituicao=f'rua_instituicao {i:02d}',
-            bairro_instituicao=f'bairro_instituicao {i:02d}',
-            cidade_instituicao=i,
-            cep_instituicao=f'cep_instituicao {i:02d}',
-            telefone=f'telefone {i:02d}'
+            f'cnpj {i:02d}', 
+            i, 
+            i,
+            f'nome {i:02d}',
+            f'email {i:02d}',
+            f'rua_instituicao {i:02d}',
+            f'bairro_instituicao {i:02d}',
+            i,
+            f'cep_instituicao {i:02d}',
+            f'telefone {i:02d}'
         )
         instituicoes.append(instituicao)
     return instituicoes
@@ -172,14 +172,14 @@ def lista_instituicoes_exemplo():
 def assinatura_exemplo():
     from data.model.assinatura_model import Assinatura
     assinatura = Assinatura(
-        cod_assinatura=1,
-        cnpj="cnpj teste",
-        cod_plano=1,
-        cod_licenca=1,
-        data_inicio=datetime(2025, 1, 1).date(),
-        data_fim=datetime(2025, 1, 1).date(),
-        valor=10.0,
-        qtd_licenca=10
+        1,
+        "cnpj teste",
+        1,
+        1,
+        datetime(2025, 1, 1).date(),
+        datetime(2025, 1, 1).date(),
+        10.0,
+        10
     )
     return assinatura
 
