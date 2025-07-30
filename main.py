1b350aa77999f85d6c4960ea6a4509e0d90f25af
LucasViguini <lucasviguinivazzoller@gmail.com>
2025-07-30 14:09:27 -0300

    Main e lauch

--- main.py
+++ main.py
@@ -238,66 +238,6 @@ async def post_novo_doador(
     else:
         return RedirectResponse("/doador", status_code=303)
 
-# @app.route('/doador/novo_doador')
-# def primeira_doacao():
-#     cod_doador = ... # recupere o código do doador logado
-#     idade_apta = verificar_idade_doador(cod_doador)
-#     return templates.TemplateResponse("primeira_doacao.html", {"request": {}, "idade_apta": idade_apta})
-
-
-
-   
-
-
-
-
-
-
-# @app.get("/admin/produtos")
-# async def get_produtos():
-#     produtos = produto_repo.obter_todos()
-#     response = templates.TemplateResponse("produtos.html", {"request": {}, "produtos": produtos})
-#     return response
-
-# @app.get("/produtos/{id}")
-# async def get_produto_por_id(id: int):
-#     produto = produto_repo.obter_por_id(id)
-#     response = templates.TemplateResponse("produto.html", {"request": {}, "produto": produto})
-#     return response
-
-# @app.get("/admin/produtos/cadastrar")
-# async def get_produto_cadastrar():
-#     response = templates.TemplateResponse("cadastrar_produto.html", {"request": {}})
-#     return response
-
-# @app.post("/admin/produtos/cadastrar")
-# async def post_produto_cadastrar(
-#     nome: str = Form(...),
-#     descricao: str = Form(...),
-#     preco: float = Form(...),
-#     quantidade: int = Form(...)
-# ):
-#     produto = Produto(0, nome, descricao, preco, quantidade)
-#     id_produto = produto_repo.inserir(produto)
-#     if id_produto == None:
-#         raise Exception("Erro ao inserir produto.")
-#     else:
-#         return RedirectResponse("/produtos", status_code=303)
-    
-
-# @app.get("/admin/clientes")
-# async def get_clientes():
-#     clientes = cliente_repo.obter_todos()
-#     response = templates.TemplateResponse("clientes.html", {"request": {}, "clientes": clientes})
-#     return response
-
-
-# @app.get("/admin/formas_pagamento")
-# async def get_formas_pagamento():
-#     formas_pagamento = forma_pagamento_repo.obter_todas()
-#     response = templates.TemplateResponse("formas_pagamento.html", {"request": {}, "formas_pagamento": formas_pagamento})
-#     return response
-
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="127.0.0.1", port=8000, reload=True)
