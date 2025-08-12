48dd6e77a99e08360da4f569c1d4ff4e648becb6
LucasViguini <lucasviguinivazzoller@gmail.com>
2025-08-12 16:37:22 -0300

    pagina de estoque

--- routes/doador/doador.py
+++ routes/doador/doador.py
@@ -32,3 +32,4 @@ async def get_doador_home(request: Request):
     response = templates.TemplateResponse("doador/doador_inicio.html", {"request": request, "active_page": "home"})
     return response
 
+
