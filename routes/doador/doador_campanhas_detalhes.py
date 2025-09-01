985a4d26212556bd33149162f13b74595ae29cda
CauaGoms <caua.privg@gmail.com>
2025-09-01 18:07:34 -0300

    Refactor campaign details page layout and styling
    - Updated HTML structure for better readability and organization.
    - Removed unnecessary sections and consolidated content into a more streamlined format.
    - Enhanced the visual presentation of campaign information, including statistics and testimonials.
    - Improved sharing functionality with updated button styles and layout.
    - Added Font Awesome for icon support and improved user interface elements.

--- routes/doador/doador_campanhas_detalhes.py
+++ routes/doador/doador_campanhas_detalhes.py
@@ -1,10 +1,19 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from data.repo import campanha_repo
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/doador/campanha/detalhes")
-async def get_doador_campanha_detalhes(request: Request):
-    response = templates.TemplateResponse("doador/doador_campanha_detalhes.html", {"request": request, "active_page": "campanha"})
-    return response
+# @router.get("/doador/campanha/detalhes")
+# async def get_doador_campanha_detalhes(request: Request):
+#     response = templates.TemplateResponse("doador/doador_campanha_detalhes.html", {"request": request, "active_page": "campanha"})
+#     return response
+
+@router.get("/doador/campanha/detalhes/{id}")
+async def get_doador_campanha_detalhes(request: Request, id: int):
+    campanha = campanha_repo.obter_por_id(id)
+    if campanha:
+        response = templates.TemplateResponse("doador/doador_campanha_detalhes.html",
+            {"request": request, "active_page": "campanha", "campanha": campanha})
+        return response
