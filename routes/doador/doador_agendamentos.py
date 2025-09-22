60b37db781b983e636833ca1d1e1912f92b1019b
CauaGoms <caua.privg@gmail.com>
2025-09-22 16:59:50 -0300

    Modificando Doador Agendamentos

--- routes/doador/doador_agendamentos.py
+++ routes/doador/doador_agendamentos.py
@@ -1,10 +1,25 @@
+import datetime
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from data.repo import agendamento_repo
 
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
 @router.get("/doador/agendamento")
 async def get_doador_agendamento(request: Request):
-    response = templates.TemplateResponse("doador/doador_agendamento.html", {"request": request, "active_page": "agendamento"})
+    agendamentos_concluidos = []
+    agendamentos_agendados = []
+    agendamentos_atrasados = []
+    agendamentos = agendamento_repo.obter_todos()
+    for agendamento in agendamentos:
+        if agendamento.status == 1:
+            agendamentos_concluidos.append(agendamento)
+        elif agendamento.status == 0:
+            if agendamento.data_hora < datetime.datetime.now():
+                agendamentos_atrasados.append(agendamento)
+            else:
+                agendamentos_agendados.append(agendamento)
+
+    response = templates.TemplateResponse("doador/doador_agendamento.html", {"request": request, "active_page": "agendamento", "agendamentos_concluidos": agendamentos_concluidos, "agendamentos_agendados": agendamentos_agendados, "agendamentos_atrasados": agendamentos_atrasados})
     return response
